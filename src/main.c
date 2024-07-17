afca3e8f3bb1ecb53e158bece13b444923ea51f3
NiKo Kompella <63625756+infection-tag@users.noreply.github.com>
2024-07-17 15:53:43 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "include/gpu.h"
 
 int main() {
+    /* prototype of an input stream */
+    int filedes = 9;
+    FILE* bbout = fdopen(filedes, "w+");
+    clear_framebuffer(bbout); /* note to self: change color to black on this */
+    
     initialize_gpu();
     render_frame();
     return 0;
